616814254359c6a30ca48dd168f45459136cb307
Tjardo18 <332688@student.mboutrecht.nl>
2023-02-12 00:48:31 +0100

    Style fix

--- index.php
+++ index.php
@@ -12,51 +12,50 @@
 </head>
 
 <body>
-    <h1 class="title">De 5 snelste achtbanen van Europa</h1>
     <div class="card">
         <h1>Invoer Achtbaan</h1>
         <form action="client/create.php" method="POST">
 
-            <div class="achtbaan">
+            <div class="inputVeld">
                 <label for="achtbaan">
                     Naam Achtbaan:
                     <input type="text" name="achtbaan" id="achtbaan">
                 </label>
             </div>
-            <div class="pretpark">
+            <div class="inputVeld">
                 <label for="pretpark">
                     Naam Pretpark:
                     <input type="text" name="pretpark" id="pretpark">
                 </label>
             </div>
-            <div class="land">
+            <div class="inputVeld">
                 <label for="land">
                     Naam Land:
                     <input type="text" name="land" id="land">
                 </label>
             </div>
-            <div class="topsnelheid">
+            <div class="inputVeld">
                 <label for="topsnelheid">
                     Topsnelheid (km/u):
                     <input type="number" name="topsnelheid" id="topsnelheid" min="1" max="200">
                 </label>
             </div>
-            <div class="hoogte">
+            <div class="inputVeld">
                 <label for="hoogte">
                     Hoogte (m):
                     <input type="number" name="hoogte" id="hoogte" min="1" max="200">
                 </label>
             </div>
-            <div class="datum">
+            <div class="inputVeld">
                 <label for="datum">
                     Datum eerste opening:
                     <input type="date" name="datum" id="datum">
                 </label>
             </div>
-            <div class="cijfer">
+            <div class="inputVeld">
                 <label for="cijfer">
                     Cijfer voor achtbaan:
-                    <div class="dis">
+                    <div class="display">
                         <input type="range" name="cijfer" id="cijfer" min="1" max="10" step=".1" oninput="getal(this.value)">
                         <p id="cijferOutput">5.5</p>
                     </div>
